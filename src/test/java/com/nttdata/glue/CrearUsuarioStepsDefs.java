95639e304c56092ad0c04183ac092f5a870e3e63
Gonzalo Uria <gonzalouriamorante_00@hotmail.com>
2024-08-08 21:55:48 -0500

    upgrade examples

--- src/test/java/com/nttdata/glue/CrearUsuarioStepsDefs.java
+++ src/test/java/com/nttdata/glue/CrearUsuarioStepsDefs.java
@@ -12,10 +12,9 @@ public class CrearUsuarioStepsDefs {
     @Steps
     CrearUsuario crearUsuario;
 
-    @When("hago la creacion del pedido con los datos {int} , {int} , {int} , {string} ,{string}, y {int}")
-    public void hagoLaCreacionDelPedidoConLosDatosY(int id, int petId , int quantity , String shipDate , String status ,  int complete ) {
+    @When("^hago la creacion del pedido con los datos id (.*) , petId (.*) , quantity (.*) , shipDate (.*), status (.*), y complete (.*)$")
+    public void hagoLaCreacionDelPedidoConLosDatosIdPetIdQuantityShipDateStatusYComplete(int id, int petId , int quantity , String shipDate , String status ,  boolean complete) {
         crearUsuario.crearpedido(id,petId,quantity,shipDate,status,complete);
-
     }
 
     @Then("el codigo de respuesta debe ser {int}")
@@ -23,18 +22,9 @@ public class CrearUsuarioStepsDefs {
         crearUsuario.validarCodigoRespuesta(codigoDeRespuesta);
     }
 
-    @And("la respuesta del body tiene que tener los datos {int} , {int} , {int} , {string} ,{string}, y {int}")
-    public void laRespuestaDelBodyTieneQueTenerLosDatosY(int id, int petId , int quantity , String shipDate , String status ,  int complete) {
-        crearUsuario.validarCuerpoRespuesta(id,petId,quantity,shipDate,status,complete);
-    }
-
-    @When("^hago la creacion del pedido con los datos id (.*) , petId (.*) , quantity (.*) , shipDate (.*), status (.*), y complete (.*)$")
-    public void hagoLaCreacionDelPedidoConLosDatosIdPetIdQuantityShipDateStatusYComplete(int id, int petId , int quantity , String shipDate , String status ,  boolean complete) {
-        crearUsuario.crearpedido2(id,petId,quantity,shipDate,status,complete);
-    }
 
     @And("^la respuesta del body tiene que tener los datos (.*),(.*),(.*),(.*),(.*),(.*)$")
     public void laRespuestaDelBodyTieneQueTenerLosDatosIdPetIdQuantityStatusComplete(int id, int petId , int quantity , String shipDate , String status ,  boolean complete) {
-        crearUsuario.validarCuerpoRespuesta2(id,petId,quantity,shipDate,status,complete);
+        crearUsuario.validarCuerpoRespuesta(id,petId,quantity,shipDate,status,complete);
     }
 }
--- src/test/java/com/nttdata/steps/CrearUsuario.java
+++ src/test/java/com/nttdata/steps/CrearUsuario.java
@@ -13,26 +13,8 @@ public class CrearUsuario {
     private Response response;
 
 
-    public void crearpedido(int id, int petId, int quantity, String shipdate, String status, int complete) {
-       response = given().baseUri(CREATE_USER)
-                .header("Content-Type","application/json")
-                .body("{\n" +
-                        "  \"id\": " + id + ",\n" +
-                        "  \"petId\": " + petId + ",\n" +
-                        "  \"quantity\": " + quantity + ",\n" +
-                        "  \"shipDate\": \"" + shipdate + "\",\n" +
-                        "  \"status\": \"" + status+ "\",\n" +
-                        "  \"complete\": " + complete + " \n" +
-                        "}")
-                .log().all()
-                .when()
-                .post(CREATE_USER)
-                .then()
-                .extract()
-                .response()
-        ;
-    }
-    public void crearpedido2(int id, int petId, int quantity, String shipdate, String status, boolean complete) {
+
+    public void crearpedido(int id, int petId, int quantity, String shipdate, String status, boolean complete) {
         response = given().baseUri(CREATE_USER)
                 .header("Content-Type","application/json")
                 .body("{\n" +
@@ -58,17 +40,7 @@ public class CrearUsuario {
     }
 
 
-    public void validarCuerpoRespuesta(int id, int petId, int quantity, String shipDate, String status, int complete) {
-        restAssuredThat(response -> response.body("'id'", equalTo(id)));
-        restAssuredThat(response -> response.body("'petId'", equalTo(petId)));
-        restAssuredThat(response -> response.body("'quantity'", equalTo(quantity)));
-        restAssuredThat(response -> response.body("'shipDate'", equalTo(shipDate)));
-        restAssuredThat(response -> response.body("'status'", equalTo(status)));
-        restAssuredThat(response -> response.body("'complete'", equalTo(complete)));
-
-    }
-
-    public void validarCuerpoRespuesta2(int id, int petId, int quantity, String shipDate, String status, boolean complete) {
+    public void validarCuerpoRespuesta(int id, int petId, int quantity, String shipDate, String status, boolean complete) {
         restAssuredThat(response -> response.body("'id'", equalTo(id)));
         restAssuredThat(response -> response.body("'petId'", equalTo(petId)));
         restAssuredThat(response -> response.body("'quantity'", equalTo(quantity)));
